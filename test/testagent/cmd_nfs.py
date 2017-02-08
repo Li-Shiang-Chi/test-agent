877f717c8b7ddbe71b9fa2b1a51a5943d4e98776
Li-Shiang-Chi <lsc830621@gmail.com>
2017-02-08 17:25:23 +0800

    modify bug

--- test/testagent/cmd_nfs.py
+++ test/testagent/cmd_nfs.py
@@ -6,4 +6,4 @@
 def clear_cluster_file(parser):
     return "sudo rm %s" % (parser["cluster_file_path"])
 def clear_node_files(parser):
-    return "sudo rm -rf %s" % (parser["primary_node_file_path"])
+    return "sudo rm -rf %s" % (parser["node_files_folder_path"])
