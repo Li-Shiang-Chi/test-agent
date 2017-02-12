84d25386929453320626fc2240bb896ca22a860a
Li-Shiang-Chi <lsc830621@gmail.com>
2017-02-12 19:55:37 +0800

    sprint 1 test

--- test/testagent/HAagent.py
+++ test/testagent/HAagent.py
@@ -133,6 +133,5 @@ def local_exec(cmd , parser):
 def remote_exec(cmd , ssh=None):
     s_stdin, s_stdout, s_stderr = ssh.exec_command("sudo "+cmd) #執行指令
     stdout = s_stdout.read()
-    print stdout
-    print s_stderr.read()
+    print stdout()
     return stdout.rstrip()
