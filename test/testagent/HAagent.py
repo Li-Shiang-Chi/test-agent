1ea000bf2db1caaebc0687cd3997f0207b22b440
Li-Shiang-Chi <lsc830621@gmail.com>
2017-02-12 21:15:49 +0800

    sprint 1 test

--- test/testagent/HAagent.py
+++ test/testagent/HAagent.py
@@ -48,8 +48,6 @@ def add_node(cluster_name , node_name , node_ip , ibmp , parser , ssh=None):
     return remote_exec(cmd, ssh) if ssh else local_exec(cmd, parser)
 
 def add_backup_node(parser , ssh):
-    print parser["BackupOS_name"]
-    
     add_node(parser["Cluster_name"],
              parser["BackupOS_name"], 
              parser["BackupOS_ip"], 
--- test/testagent/process.py
+++ test/testagent/process.py
@@ -366,11 +366,6 @@ def exec_add_node(parser):
 	
 	HAagent.create_cluster(parser["Cluster_name"], parser["HostOS_name"], parser["HostOS_ipmb"], parser["Shelf_ip"], parser, ssh)
 	time.sleep(3)
-	ssh.close()
-	
-	ssh = shell_server.get_ssh(parser["HostOS_ip"]
-                              , parser["HostOS_usr"]
-                              , parser["HostOS_pwd"]) #獲得ssh
 	HAagent.add_backup_node(parser , ssh) # add backup node
 	time.sleep(float(parser["pro_wait_add_node_time"])) 
 	#HAagent.add_slave_node(parser , ssh) #add slave node
@@ -458,5 +453,7 @@ if __name__ == '__main__':
 	ssh = shell_server.get_ssh(parser["HostOS_ip"]
                               , parser["HostOS_usr"]
                               , parser["HostOS_pwd"]) #獲得ssh
-	
+	HAagent.create_cluster(parser["Cluster_name"], parser["HostOS_name"], parser["HostOS_ipmb"], parser["Shelf_ip"], parser, ssh)
+	#time.sleep(3)
 	HAagent.add_backup_node(parser, ssh)
+	#time.sleep(5)
