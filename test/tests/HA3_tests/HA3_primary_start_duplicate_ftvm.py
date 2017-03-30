eba679742185efc53d3fc5d138f35f8c179245be
Li-Shiang-Chi <lsc830621@gmail.com>
2017-03-30 20:18:24 +0800

    sprint 3 test

--- test/tests/HA3_tests/HA3_primary_start_duplicate_ftvm.py
+++ test/tests/HA3_tests/HA3_primary_start_duplicate_ftvm.py
@@ -6,5 +6,5 @@ from testagent import preprocess
 def run_HA3_primary_start_duplicate_ftvm(parser):
     preprocess.run_preprocess(parser)
     process.host_vm_ftstart(parser)
-    Assert.vm_recover_in_primaryOS(parser)
     Assert.vm_duplicate_start(parser)
+    Assert.vm_recover_in_primaryOS(parser)
