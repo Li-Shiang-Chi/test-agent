3417dc1c2e1513b4bf750e0e0ec06546e476ddc4
Li-Shiang-Chi <lsc830621@gmail.com>
2017-05-04 17:16:52 +0800

    sprint 3 test

--- test/tests/HA3_tests/HA3_primary_network_isolation.py
+++ test/tests/HA3_tests/HA3_primary_network_isolation.py
@@ -2,13 +2,14 @@ from testagent import preprocess
 from testagent import process
 from testagent import Assert
 from testagent import postprocess
+import time
 
 def run_HA3_primary_network_isolation(parser):
     """
     general architecture hostOS crash
     """
     #preprocess
-    preprocess.preprocess(parser)
+    preprocess.run_preprocess(parser)
     print 401
     #process
     process.exec_primaryOS_network_isolation(parser)
@@ -19,6 +20,7 @@ def run_HA3_primary_network_isolation(parser):
     print 404
     #Assert.detect_primary_network_isolation_info(parser)
     print 405
+    time.sleep(30)
     Assert.primaryOS_role_is_slave(parser)
     print 406
     Assert.backupOS_role_is_primary(parser)
