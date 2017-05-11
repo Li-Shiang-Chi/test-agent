521d19449ecf9e399220af1c1a65e2732f98dcb8
Li-Shiang-Chi <lsc830621@gmail.com>
2017-05-11 16:42:23 +0800

    sprint 3 test

--- test/tests/HA3_tests/HA3_primary_network_isolation.py
+++ test/tests/HA3_tests/HA3_primary_network_isolation.py
@@ -2,7 +2,6 @@ from testagent import preprocess
 from testagent import process
 from testagent import Assert
 from testagent import postprocess
-import time
 
 def run_HA3_primary_network_isolation(parser):
     """
@@ -20,7 +19,6 @@ def run_HA3_primary_network_isolation(parser):
     print 404
     #Assert.detect_primary_network_isolation_info(parser)
     print 405
-    time.sleep(60)
     Assert.primaryOS_role_is_slave(parser)
     print 406
     Assert.backupOS_role_is_primary(parser)
